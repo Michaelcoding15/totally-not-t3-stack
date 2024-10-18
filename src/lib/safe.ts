10035d08175f96f8e9e1acf79f63a6d180bb382f
TigerQa <mickelbick@gmail.com>
2024-10-18 10:14:07 -0400

    Grammer

--- src/lib/safe.ts
+++ src/lib/safe.ts
@@ -1,5 +1,5 @@
 /**
- * This function will try and catch a passed in function, and return either the result or the error depending on if the function succeeds.
+ * This function will try and catch a passed in function, and return either the result or an error depending on if the function succeeds.
  * @template T
  * @template E
  * @param {() => T} func The function you want to pass in.
@@ -19,7 +19,7 @@ export function safeTry<T, E = Error>(func: () => T): [null, T] | [E, null] {
 }
 
 /**
- * This function will try and catch a passed in async function, and return either the result or the error depending on if the function succeeds.
+ * This function will try and catch a passed in async function, and return either the result or an error depending on if the function succeeds.
  * @template T
  * @template E
  * @param {() => Promise<T>} func The async function you want to pass in.
